2d948a92250ac8517f650be6941051d972fabcd8
Marwan El-Adawy <MarwanELAdawy@users.noreply.github.com>
2020-05-02 21:30:10 +0200

    Last Update

--- public/recognition.js
+++ public/recognition.js
@@ -1,30 +1,53 @@
-import e, { response } from "express";
+// import e from "express";
+// import  response from 'express';
 
 document.addEventListener('DOMContentLoaded', speechToEmotion, false);
-
-const recognition = new webkitSpeechRecognition();
-
-recognition.lang = 'en-US';
-recognition.continuous = true;
-
-recognition.onresult = (event)=>{
-    const results = event.results;
-    const transcript = results[results.length-1][0].transcript;
-
-    fetch(`/emotion?text=${transcript}`)
-      .then((response)=>response.json())
-      .then((result)=>{
-        console.log('result ->', result);
-      })
-      .catch((e)=>{
-        console.error('Request error -> ', e);
-      });
-};
-recognition.onend = ()=> {
-    console.log('disconnected');
-};
-recognition.start();
-
 function speechToEmotion() {
-  // Web Speech API section code will be added here
+    const recognition = new webkitSpeechRecognition();
+
+    recognition.lang = 'en-US';
+    recognition.continuous = true;
+
+    const setEmoji = (type) =>  {
+        const emojiElem = document.querySelector('.emoji img');
+        emojiElem.classList = type;
+    };
+
+    recognition.onresult = (event)=>{
+        const results = event.results;
+        const transcript = results[results.length-1][0].transcript;
+
+        fetch(`/emotion?text=${transcript}`)
+        .then((response)=>response.json())
+        .then((result) => {
+            if (result.score > 0) {
+            setEmoji('positive');
+            } else if (result.score < 0) {
+            setEmoji('negative');
+            } else {
+            setEmoji('listening');
+            }
+        })
+        .catch((e)=>{
+            console.error('Request error -> ', e);
+            recognition.abort();
+        });
+    };
+    recognition.onend = ()=> {
+        console.log('disconnected');
+    };
+    recognition.onerror = (event) => {
+        console.error('Recognition error -> ', event.error);
+        setEmoji('error')
+    };
+
+    recognition.onaudiostart = ()=> {
+        setEmoji('listening');
+    };
+
+    recognition.onend = ()=> {
+        setEmoji('idle');
+    };
+
+    recognition.start();
 }
